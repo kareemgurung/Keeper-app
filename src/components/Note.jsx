f96a8bae4e357f8fe16967aaab9383292f406dd2
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-10-31 12:38:12 +0000

    Added header , note and footer with some css style

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Coding Practice</h1>
+            <p>Create a react app from scratch.</p>
+        </div>
+    )
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '../src/components/App.jsx';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <App />,
   document.getElementById('root')
 );
 
